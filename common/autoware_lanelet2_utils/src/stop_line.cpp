660a0b91f4ed1b7d3cff15bc4d01b29f09b0c7a7
mraditya01 <mraditya01@yahoo.com>
2025-04-04 16:20:36 +0900

    remove PathWithLinkedID function and added some other function

--- common/autoware_lanelet2_utils/src/stop_line.cpp
+++ common/autoware_lanelet2_utils/src/stop_line.cpp
@@ -23,76 +23,11 @@
 
 namespace autoware::lanelet2_utils
 {
-
-template <typename GeometryT>
-std::optional<std::pair<size_t, Point2d>> find_first_intersection(
-  const PathWithLaneId & path, const std::pair<size_t, size_t> path_range,
-  const GeometryT & geometry)
-{
-  for (size_t i = path_range.first; i < path_range.second && i < path.points.size() - 1; ++i) {
-    const auto & p_front = path.points.at(i).point.pose.position;
-    const auto & p_back = path.points.at(i + 1).point.pose.position;
-    const LineString2d path_segment = {{p_front.x, p_front.y}, {p_back.x, p_back.y}};
-
-    std::vector<Point2d> collision_points;
-    boost::geometry::intersection(geometry, path_segment, collision_points);
-
-    if (!collision_points.empty()) {
-      return std::make_pair(i, collision_points.front());
-    }
-  }
-  return std::nullopt;
-}
-
-std::optional<LineString2d> generate_stop_line(
-  const PathWithLaneId & path, const lanelet::ConstPolygons3d & no_stopping_areas,
-  const double ego_width, const double stop_line_margin)
-{
-  for (const auto & no_stopping_area : no_stopping_areas) {
-    const auto & area_poly = lanelet::utils::to2D(no_stopping_area).basicPolygon();
-
-    std::pair<size_t, size_t> path_range = {0, path.points.size() - 1};
-    auto intersection_result = find_first_intersection(path, path_range, area_poly);
-
-    if (intersection_result) {
-      size_t i = intersection_result->first;
-      const Point2d & collision_point = intersection_result->second;
-
-      const auto p0 = path.points.at(i).point.pose.position;
-      const auto p1 = path.points.at(i + 1).point.pose.position;
-      const double yaw = autoware::universe_utils::calcAzimuthAngle(p0, p1);
-      const double w = ego_width;
-      const double l = stop_line_margin;
-
-      LineString2d stop_line;
-      stop_line.emplace_back(
-        -l * std::cos(yaw) + collision_point.x() + w * std::cos(yaw + M_PI_2),
-        collision_point.y() + w * std::sin(yaw + M_PI_2));
-      stop_line.emplace_back(
-        -l * std::cos(yaw) + collision_point.x() + w * std::cos(yaw - M_PI_2),
-        collision_point.y() + w * std::sin(yaw - M_PI_2));
-      return stop_line;
-    }
-  }
-  return {};
-}
-
 universe_utils::LineString2d get_stop_line_geometry2d(
   const lanelet::ConstLineString3d & stop_line, const double extend_length)
 {
   return autoware::behavior_velocity_planner::planning_utils::extendLine(
     stop_line[0], stop_line[1], extend_length);
-}
-
-std::optional<LineString2d> get_stop_line_geometry2d(
-  const PathWithLaneId & path, const lanelet::Optional<lanelet::ConstLineString3d> & stop_line,
-  const lanelet::ConstPolygons3d & no_stopping_areas, const double stop_line_margin,
-  const double stop_line_extend_length, const double vehicle_width)
-{
-  if (stop_line && stop_line->size() >= 2) {
-    return get_stop_line_geometry2d(stop_line.value(), stop_line_extend_length);
-  }
-  return generate_stop_line(path, no_stopping_areas, vehicle_width, stop_line_margin);
 }
 
 std::vector<lanelet::ConstLineString3d> get_stop_lines_from_stop_sign(
@@ -125,7 +60,7 @@ std::vector<lanelet::ConstLineString3d> get_stop_lines_from_stop_sign(
   return stoplines;
 }
 
-std::optional<lanelet::ConstLineString3d> get_stop_line_from_map(
+std::optional<lanelet::ConstLineString3d> get_stop_line_from_road_marking(
   const lanelet::Id lane_id, const lanelet::LaneletMapPtr & lanelet_map_ptr,
   const std::string & attribute_name, bool check_id_match)
 {
@@ -155,35 +90,55 @@ std::optional<lanelet::ConstLineString3d> get_stop_line_from_map(
   return stop_line.front();
 }
 
-std::optional<size_t> get_stop_line_index_from_map(
-  const PathWithLaneId & path, const std::pair<size_t, size_t> lane_interval,
-  lanelet::ConstLanelet assigned_lanelet, const lanelet::LaneletMapPtr & lanelet_map_ptr,
-  double stop_line_extend_length, double nearest_dist_threshold, double nearest_yaw_threshold)
+std::vector<lanelet::ConstLineString3d> get_stop_lines_from_no_stopping_area(
+  const lanelet::ConstLanelets & lanelets)
 {
-  auto stopline_opt = get_stop_line_from_map(assigned_lanelet.id(), lanelet_map_ptr, "", false);
+  std::vector<lanelet::ConstLineString3d> stopLines;
+  std::set<lanelet::Id> checklist;
 
-  if (!stopline_opt) {
-    return std::nullopt;
+  for (const auto & ll : lanelets) {
+    std::vector<std::shared_ptr<const lanelet::autoware::NoStoppingArea>> no_stopping_elems =
+      ll.regulatoryElementsAs<lanelet::autoware::NoStoppingArea>();
+
+    if (!no_stopping_elems.empty()) {
+      for (const auto & no_stopping_area : no_stopping_elems) {
+        if (auto opt_stop_line = no_stopping_area->stopLine()) {
+          lanelet::ConstLineString3d stopLine = *opt_stop_line;
+          auto id = stopLine.id();
+          if (checklist.find(id) == checklist.end()) {
+            checklist.insert(id);
+            stopLines.push_back(stopLine);
+          }
+        }
+      }
+    }
+  }
+  return stopLines;
+}
+
+std::vector<lanelet::ConstLineString3d> get_stop_lines_from_detection_area(
+  const lanelet::ConstLanelets & lanelets)
+{
+  std::vector<lanelet::ConstLineString3d> stopLines;
+  std::set<lanelet::Id> checklist;
+
+  for (const auto & ll : lanelets) {
+    std::vector<std::shared_ptr<const lanelet::autoware::DetectionArea>> detection_areas =
+      ll.regulatoryElementsAs<lanelet::autoware::DetectionArea>();
+
+    if (!detection_areas.empty()) {
+      for (const auto & detection_area : detection_areas) {
+        const lanelet::ConstLineString3d stopLine = detection_area->stopLine();
+        const lanelet::Id id = stopLine.id();
+        if (checklist.find(id) == checklist.end()) {
+          checklist.insert(id);
+          stopLines.push_back(stopLine);
+        }
+      }
+    }
   }
 
-  const auto stopline = *stopline_opt;
-  const LineString2d extended_stopline =
-    get_stop_line_geometry2d(stopline, stop_line_extend_length);
-
-  auto intersection_result = find_first_intersection(path, lane_interval, extended_stopline);
-  if (intersection_result) {
-    return intersection_result->first;
-  }
-
-  const auto p_start = stopline.front();
-  const auto p_end = stopline.back();
-  geometry_msgs::msg::Pose stop_point_from_map;
-  stop_point_from_map.position.x = 0.5 * (p_start.x() + p_end.x());
-  stop_point_from_map.position.y = 0.5 * (p_start.y() + p_end.y());
-  stop_point_from_map.position.z = 0.5 * (p_start.z() + p_end.z());
-
-  return autoware::motion_utils::findFirstNearestIndexWithSoftConstraints(
-    path.points, stop_point_from_map, nearest_dist_threshold, nearest_yaw_threshold);
+  return stopLines;
 }
 
 }  // namespace autoware::lanelet2_utils
